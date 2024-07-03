b6474e3e6e175401f9434f3e2ebbb6072e100a64
gitesh44 <deshmukhgitesh44gmail.com>
2024-07-03 11:34:12 +0530

    Trial

---
+++ Array/MaximumValueInArr.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class MaximumValueInArr {
+    public static void main(String[] args) {
+        int[] arr={23,4,77,865,66,98,5543,2234,654};
+        System.out.println(max(arr));
+    }
+
+    public static int max(int[] arr) {
+
+        int maxv=arr[0];
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index]>maxv){
+                maxv=arr[index]; 
+            }
+            
+        }
+        return maxv;
+        
+
+        
+    }
+}
--- Array/Swap.java
+++ Array/Swap.java
@@ -9,10 +9,16 @@ public class Swap {
         swap(arr,0,3);
 
         System.out.println(Arrays.toString(arr));
-        /*  public static void main(String[] args): This is the entry point of the program.
-         int[] arr = {1, 3, 4, 22, 55, 35};: This initializes an array arr with six integers.
-         swap(arr, 0, 3);: This calls the swap method to swap the elements at indices 0 and 3 in the array arr.
-         System.out.println(Arrays.toString(arr));: This prints the array after the swap operation using Arrays.toString to convert the array to a string. */
+
+
+        //_________________LOGIC__________
+        /*
+
+
+        -public static void main(String[] args): This is the entry point of the program.
+        -int[] arr = {1, 3, 4, 22, 55, 35};: This initializes an array arr with six integers.
+        -swap(arr, 0, 3);: This calls the swap method to swap the elements at indices 0 and 3 in the array arr.
+        -System.out.println(Arrays.toString(arr));: This prints the array after the swap operation using Arrays.toString to convert the array to a string. */
 
 
 
@@ -21,7 +27,13 @@ public class Swap {
             int temp=arr[index];
             arr[index]=arr[index2];
             arr[index2]=temp;
+     
 
+            /* 
+            -static void swap(int[] arr, int index1, int index2): This is a static method named swap that takes three parameters: an array arr, and two integers index1 and index2.
+            -int temp = arr[index1];: This stores the value at index1 in a temporary variable temp.
+            -arr[index1] = arr[index2];: This assigns the value at index2 to the position index1.
+            -arr[index2] = temp;: This assigns the value stored in temp to the position index2. */
 
 
     };
